5700ab1b252f73e7769bfc70e71547a72b5784ee
jojo4x4 <jonathan.balraj@gmail.com>
2022-01-18 21:00:25 -0800

    added a ping command

--- index.js
+++ index.js
@@ -8,4 +8,15 @@ client.on('ready', () => {
     console.log('Connected');
 });
 
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+        const then = Date.now();
+		await interaction.reply('Pong!').then(() => interaction.editReply(`Pong! It took ${Date.now() - then}ms to send`));
+	} 
+});
+
 client.login(token);
