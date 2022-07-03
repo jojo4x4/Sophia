f37d2800d1024d8cf28a208b6f514afb99d55b72
jojo4x4 <jonathan.balraj@gmail.com>
2022-07-03 16:22:33 -0700

    clear voice text channel when all users disconnect

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { Client, Intents, Collection } = require('discord.js');
 const { token } = require('./config.json');
 const fs = require('fs');
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_VOICE_STATES] });
 client.commands = new Collection();
 
 // Add all commands from commands directory
@@ -28,6 +28,14 @@ client.on('interactionCreate', async interaction => {
 	} catch (error) {
 		console.error(error);
 		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
+
+client.on('voiceStateUpdate', async (oldState, newState) => {
+	// clear Voice Text channel when everyone disconnects
+	if (oldState.channel != null && newState.channel == null) {
+		while (await oldState.channel.messages.fetch().then(messages => messages.size) > 1) 
+			oldState.channel.bulkDelete(100);
 	}
 });
 
